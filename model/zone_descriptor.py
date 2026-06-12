1fd6fd9fb5d72481460cea25b16e88a4c536d83c
STEFANIAGT <153624694+STEFANIAGT@users.noreply.github.com>
2026-06-12 14:30:14 +0200

    Add ZoneDescriptor class for vineyard zones

--- model/zone_descriptor.py
+++ model/zone_descriptor.py
@@ -1,1 +1,18 @@
 
+import json
+
+
+class ZoneDescriptor:
+    """
+    Rappresenta una zona del vigneto (Smart Object).
+    Pubblicato come messaggio retained su: vigneto/<zone_id>/info
+    """
+
+    def __init__(self, zone_id, zone_name, producer, software_version):
+        self.zone_id = zone_id
+        self.zone_name = zone_name
+        self.producer = producer
+        self.software_version = software_version
+
+    def to_json(self):
+        return json.dumps(self, default=lambda o: o.__dict__)
