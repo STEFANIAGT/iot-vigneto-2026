dac377fa4b347c4180653138ef5a98b4e5ca66f3
STEFANIAGT <153624694+STEFANIAGT@users.noreply.github.com>
2026-06-12 12:52:06 +0200

    Add MessageDescriptor class for telemetry messages

--- model/message_descriptor.py
+++ model/message_descriptor.py
@@ -1,1 +1,18 @@
 
+import json
+import time
+
+
+class MessageDescriptor:
+    """
+    Tipologia generica per messaggi di telemetria o comandi.
+    Contiene: tipo di messaggio, payload e timestamp.
+    """
+
+    def __init__(self, message_type, value):
+        self.message_type = message_type
+        self.value = value
+        self.timestamp = int(time.time())
+
+    def to_json(self):
+        return json.dumps(self, default=lambda o: o.__dict__)
